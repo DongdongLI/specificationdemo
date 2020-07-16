c653f04303bf2a7ca2884d754e9c2d6f08c0090c
Don.Li <dongdong.li@adara.com>
2020-07-16 18:32:29 -0400

    add or() inside specification interface

--- src/main/java/criteria/Specification.java
+++ src/main/java/criteria/Specification.java
@@ -5,4 +5,5 @@ import model.Entry;
 public interface Specification {
     boolean isSatisfied(Entry entry);
     Specification and(Specification specification);
+    Specification or(Specification specification);
 }
